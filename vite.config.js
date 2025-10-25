74623b6a4700e3d0b258665da1daee91b2b38c29
Charishma <2300030321cseh@gmail.com>
2025-10-25 09:10:19 +0530

    Fix GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Replace <your-github-username> and <repo-name>
 export default defineConfig({
   plugins: [react()],
+  base: '/carrental-frontend/', // 👈 required for GitHub Pages
 })
